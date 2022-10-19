e8763388c5c858e208cf0426ee1490e72db8f3ae
Alejksejejv Vladislav <39629598+Zestiko@users.noreply.github.com>
2022-10-19 15:37:21 +0300

    finish first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,60 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+    gallery: document.querySelector('.gallery'),
+};
+const { gallery,bigImage } = refs;
+// console.log(galleryItems);
+// console.log(refs.gallery);
+
+const galleryItemsRefs = galleryItems.map(({preview, original, description}) => `
+    <a class="gallery__item" href="#">
+        <img class="gallery__image" src="${preview}" 
+        alt="${description}" data-source="${original}">
+    </a> `).join('');
+// console.log(galleryItemsRefs)
+
+gallery.innerHTML = galleryItemsRefs;
+gallery.addEventListener('click', onClickGallary);
+
+
+
+function onClickGallary(evt) {
+    evt.preventDefault();
+    if ("IMG" !== evt.target.nodeName) {
+        return
+    }
+    console.log(evt.target.dataset.source);
+    const bigImage = basicLightbox.create(`
+    <img
+     src="${evt.target.dataset.source}"
+    alt="${evt.target.alt}"
+        width="800"
+        height="600">
+`);
+bigImage.show()
+
+function handelEscape(evt) {
+    if (evt.key !== 'Escape') {
+        return
+    }
+    bigImage.close();
+    window.removeEventListener('keydown', handelEscape);
+    window.removeEventListener('mousedown', closeWindowOnClick);
+    console.log(evt.key)
+};
+function closeWindowOnClick(evt) {
+    window.removeEventListener('keydown', handelEscape);
+    window.removeEventListener('mousedown', closeWindowOnClick);
+    console.log(evt.button)
+};
+window.addEventListener('keydown', handelEscape);
+window.addEventListener('mousedown', closeWindowOnClick);
+    
+};
+
+
+
+
+    
